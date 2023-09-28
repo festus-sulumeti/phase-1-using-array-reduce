505c687c2bac8ce47a6a3924340bd6471f2ff99c
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-29 02:33:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function reducer(total, element){
+  return total + element;
+}
+
+let totalBatteries = batteryBatches.reduce(reducer, 0);
+
+console.log(totalBatteries);
